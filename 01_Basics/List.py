e652ed115729e0ee4aca85249adfd45553a59ee4
ALCOR419 <ashishmore1235@gmail.com>
2026-09-21 20:00:02 +0530

    Lists are used to store multiple items in a single variable

---
+++ 01_Basics/List.py
@@ -0,0 +1,29 @@
+#Create a List
+
+ashish = ["Smart", "handsome" , "good looking"]
+print(ashish)
+
+
+#Allow Duplicates
+
+ashish = ["Smart", "handsome" , "good looking" , "handsome"]
+print(ashish)
+
+#List Length
+print(len(ashish))
+
+#List Items - Data Types
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1 , list2 ,list3)
+
+#A list can contain different data types
+
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+
+#From Python's perspective, lists are defined as objects with the data type 'list
+
+print(type(ashish))
