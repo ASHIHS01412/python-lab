39381b1a421c57add936f78ad44f1a33b8ff3a8e
ALCOR419 <ashishmore1235@gmail.com>
2026-09-22 21:57:06 +0530

    To change the value of a specific item, refer to the index numbe

---
+++ 01_Basics/Change_List.py
@@ -0,0 +1,21 @@
+#Change Item Value
+ashish = ["Smart", "handsome" , "good looking", "Intelligent" ]
+ashish[1] = "Kind"
+print(ashish)
+
+#Change a Range of Item Values
+
+ashish[1:3] = ["handsome","kind"]
+print(ashish)
+
+
+ashish[1:2] = ["problem-solving","Motivated "]
+print(ashish)
+
+ashish[1:3] = ["god"]
+print(ashish)
+
+#Insert Items
+
+ashish.insert(2,"good looking")
+print(ashish)
